3257b794245aedbfcaa980de9f3b4743954e46c4
Maksim Kozlov <max969932@gmail.com>
2019-10-24 15:43:30 +0300

    add lab xUnit

---
+++ xUnit/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public static bool DoesTheTriangleExist(float side_a, float side_b, float side_c)
+        {
+            if (side_a + side_b <= side_c)
+                return false;
+            if (side_b + side_c <= side_a)
+                return false;
+            if (side_c + side_a <= side_b)
+                return false;
+            if (side_a <= 0)
+                return false;
+            if (side_b <= 0)
+                return false;
+            if (side_c <= 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ xUnit/TriangleTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sideA_0_and_other_side_greater_than_0()
+        {
+            //arrange
+            float sideA = 0;
+            float sideB = 20;
+            float sideC = 30;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sum_sideA_and_sideB_is_not_greater_than_sideC()
+        {
+            //arrange
+            float sideA = 1;
+            float sideB = 2;
+            float sideC = 3;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sideA_less_than_0()
+        {
+            //arrange
+            float sideA = -2;
+            float sideB = 20;
+            float sideC = 30;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sideA_less_than_0_and_sideB_equally_0()
+        {
+            //arrange
+            float sideA = -5;
+            float sideB = 0;
+            float sideC = 30;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sum_sideB_and_sideC_is_not_greater_than_sideA()
+        {
+            //arrange
+            float sideA = 10;
+            float sideB = 2;
+            float sideC = 3;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sum_sideA_and_sideB_equally_sideC()
+        {
+            //arrange
+            float sideA = 1;
+            float sideB = 2;
+            float sideC = 3;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sum_sideA_and_sideC_is_not_greater_than_sideB()
+        {
+            //arrange
+            float sideA = 1;
+            float sideB = 3;
+            float sideC = 2;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sideA_0_sideB_0_sideC_0()
+        {
+            //arrange
+            float sideA = 0;
+            float sideB = 0;
+            float sideC = 0;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_sideA_3_sideB_4_sideC_5()
+        {
+            //arrange
+            float sideA = 3;
+            float sideB = 4;
+            float sideC = 5;
+            bool expected = true;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DoesTheTriangleExist_if_all_sides_less_than_0()
+        {
+            //arrange
+            float sideA = -1;
+            float sideB = -2;
+            float sideC = -3;
+            bool expected = false;
+
+            //act
+            bool actual = Triangle.DoesTheTriangleExist(sideA, sideB, sideC);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
