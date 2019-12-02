cd2b38cbb42dd272002f1c6d70b3d5ab98c7782e
Maksim Kozlov <max969932@gmail.com>
2019-12-02 22:58:29 +0300

    Add PageObject lab

---
+++ PageObject/PageObject/GdTicketsHomePage.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObject
+{
+    class GdTicketsHomePage
+    {
+        private readonly IWebDriver _driver;
+
+        [FindsBy(How = How.XPath, Using = "//table[@class='ui-datepicker-calendar']")]
+        private IWebElement calendar;
+
+        public GdTicketsHomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        public IWebElement arriveCity => GetWebElementById("arrival");
+
+        public IWebElement departureCity => GetWebElementById("departure");
+
+        public IWebElement searchButton => GetWebElement("//button[@data-uil='submit_search']");
+
+        public void ChoiceDepartureCity(string city)
+        {
+            departureCity.SendKeys(city);
+            IWebElement _departureCity = GetWebElement($"//a[@class='ui-menu__item ui-menu__item--pin ui-corner-all' and text()='{city}']");
+            _departureCity.Click();
+        }
+
+        public void ChoiceArriveCity(string city)
+        {
+            arriveCity.SendKeys(city);
+            IWebElement _arriveCity = GetWebElement($"//a[@class='ui-menu__item ui-menu__item--pin ui-corner-all ui-state-focus' and text()='{city}']");
+            _arriveCity.Click();
+        }
+
+        public string getErrorMessage()
+        {
+            return GetWebElement("//samp[@generated='true' and @for='departure_date' and @class='error _idx_2']").Text;
+        }
+
+        public IEnumerable<IWebElement> GetCalendarDates()
+        {
+            var datesList = GetWebElement("//table[@class='ui-datepicker-calendar']");
+            return datesList.FindElements(By.XPath("//a"));
+        }
+
+        public void OpenCalendar()
+        {
+            calendar.Click();
+        }
+
+        private IWebElement GetWebElement(string xPath)
+        {
+            return _driver.FindElement(By.XPath(xPath));
+        }
+
+        private IWebElement GetWebElementById(string Id)
+        {
+            return _driver.FindElement(By.Id(Id));
+        }
+    }
+}
---
+++ PageObject/PageObject/Tests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObject
+{
+    [TestFixture]
+    public class Tests
+    {
+        public IWebDriver webDriver;
+
+        [SetUp]
+        public void OpenBrowserAndGoToSite()
+        {
+            webDriver = new OperaDriver("C:\\WebDriver\\bin\\");
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            webDriver.Navigate().GoToUrl("https://gd.tickets.ua/");
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            webDriver.Quit();
+        }
+
+        // Найти билет, не указав дату.
+        //-зайти на https://gd.tickets.ua
+        //-откуда: Киев
+        //-куда: Харьков-Пасс
+        //-дата: поле оставить пустым
+        //
+        //*ожидаемый результат: вывод сообщения - 'Это поле необходимо заполнить'
+        [Test]
+        public void FindTicketWithEmptyFieldDepartureDate()
+        {
+            var homepage = new GdTicketsHomePage(webDriver);
+
+            #region TestData
+            const string departureCityText = "Киев";
+            const string arriveCityText = "Харьков-Пасс";
+            const string expectedErrorMessage = "Это поле необходимо заполнить";
+            #endregion
+
+            homepage.ChoiceDepartureCity(departureCityText);
+            homepage.ChoiceArriveCity(arriveCityText);
+            homepage.searchButton.Click();
+
+            Assert.AreEqual(expectedErrorMessage, homepage.getErrorMessage());
+        }
+
+        // Найти билет, указав вчерашнюю дату
+        //-зайти на https://gd.tickets.ua
+        //-откуда: Киев
+        //-куда: Харьков-Пасс
+        //-дата: вчерашняя
+        //-нажать "найти"
+        //
+        //*ожидаемый результат: вывод сообщения - 'Это поле необходимо заполнить'
+        [Test]
+        public void FindTicketWithYeasterdayDate()
+        {
+            var homepage = new GdTicketsHomePage(webDriver);
+            #region TestData
+            const string departureCityText = "Киев";
+            const string arriveCityText = "Харьков-Пасс";
+            const string expectedErrorMessage = "Это поле необходимо заполнить";
+            int yesterday = DateTime.Now.Day - 1;
+            #endregion
+
+            homepage.ChoiceDepartureCity(departureCityText);
+            homepage.ChoiceArriveCity(arriveCityText);
+            homepage.OpenCalendar();
+            var dates = homepage.GetCalendarDates();
+            var day = dates.FirstOrDefault(d => d.Text.Equals(yesterday.ToString()));
+            day.Click();
+            homepage.searchButton.Click();
+
+            Assert.AreEqual(expectedErrorMessage, homepage.getErrorMessage());
+        }
+    }
+}
